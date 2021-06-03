36aec4dd4ea70b3f1940ba1b5534445d4d7e9006
Martin Balke <Mrtnbalke@gmail.com>
2021-06-03 11:05:56 -0700

    updated input functionality so that click off populates text

--- src/components/WeatherContainer.js
+++ src/components/WeatherContainer.js
@@ -1,48 +1,56 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import superagent from 'superagent';
 import { formatWeatherData } from '../helpers/formatWeatherData';
 import LocationDataDisplay from './LocationDataDisplay';
-import ForecastDataDisplay from './ForecastDataDisplay'
+import ForecastDataDisplay from './ForecastDataDisplay';
 
 const WeatherContainer = () => {
-  const [weatherData, setWeatherData] = useState('');
-  const [location, setLocation] = useState('Seattle')
+	const [weatherData, setWeatherData] = useState('');
+	const [location, setLocation] = useState('Seattle');
+  const inputRef = useRef(false);
+  
   useEffect(() => {
-    async function getWeatherData() {
-      try {
-       let data = await superagent.get(
-					`http://api.weatherapi.com/v1/forecast.json?key=763268107e8e427abe0160826210206&q=${location}&days=7&aqi=no&alerts=no`
-        );
-        if (data.status === 200) {
-          data = formatWeatherData(data.body);
-          setWeatherData(data);
-        };
-      } catch (error) {
-        console.log('searching')
-      }
-     
-     
-    }
+    window.addEventListener('click', e => {
+      if (e.target !== inputRef.current) {inputRef.current.value = weatherData.locationData.name}
+     })
 
-    getWeatherData();
+  })
 
-  },[location])
+	useEffect(() => {
+		async function getWeatherData() {
+			try {
+				let data = await superagent
+					.get(
+						`http://api.weatherapi.com/v1/forecast.json?key=763268107e8e427abe0160826210206&q=${location}&days=7&aqi=no&alerts=no`
+					)
+				if (data.status === 200) {
+					data = formatWeatherData(data.body);
+					setWeatherData(data);
+				}
+			} catch (error) {}
+		}
 
-  return (
-    <div style={{ width:'50%'} }>
-      <input type="text" onKeyUp={ e => setLocation( () => e.target.value)} on/>
+		getWeatherData();
+	}, [location]);
+
+	return (
+		<div style={{ width: '50%' }}>
+			<input
+				type='text'
+				onKeyUp={(e) => setLocation(() => e.target.value)}
+				ref={inputRef}
+			/>
 			{weatherData.locationData && (
 				<LocationDataDisplay location={weatherData.locationData} />
-      )}
-      <div className="" style={{display:'flex'} }>
-
-        {weatherData.forecastData && weatherData.forecastData.map((forecast) => (
-          <ForecastDataDisplay forecast={forecast} />
-        ))}
-      </div>
+			)}
+			<div className='' style={{ display: 'flex' }}>
+				{weatherData.forecastData &&
+					weatherData.forecastData.map((forecast) => (
+						<ForecastDataDisplay forecast={forecast} />
+					))}
+			</div>
 		</div>
 	);
 };
 
 export default WeatherContainer;
-
