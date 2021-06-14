921620bbb8aa6c7c03d4bdbb07cb0363cfe962f1
Martin Balke <Mrtnbalke@gmail.com>
2021-06-13 17:42:30 -0700

    converted the searchbar over to tsx

---
+++ src/components/LocationSearchBar.tsx
@@ -0,0 +1,40 @@
+import React, { useRef, useEffect} from 'react';
+import { Flex, Input, InputGroup, InputRightAddon } from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
+import { FormattedWeatherData } from '../helpers/formatWeatherData';
+
+interface Props {
+	setLocation: Function,
+	weatherData: FormattedWeatherData
+}
+
+const LocationSearchBar = ({ setLocation, weatherData } : Props) => {
+	const inputRef = useRef < HTMLInputElement>(null)
+
+  useEffect(() => {
+    function handleClickOutside(e : Event) {
+      if (e.target !== inputRef.current && weatherData.locationData?.name) {
+        inputRef.current!.value = weatherData.locationData?.name;
+      }
+    }
+    window.addEventListener('click', handleClickOutside);
+
+    return () => window.removeEventListener('click',handleClickOutside);
+  });
+  return (
+		<Flex>
+			<InputGroup>
+				<Input
+					placeholder='Search'
+					onKeyUp={(event: React.KeyboardEvent<HTMLInputElement>) =>
+						setLocation(() => (event.target as HTMLInputElement).value)
+					}
+					ref={inputRef}
+				/>
+				<InputRightAddon children={<SearchIcon />} />
+			</InputGroup>
+		</Flex>
+	);
+};
+
+export default LocationSearchBar;
